9868b537ca91340744397d0366975e49b89d86cc
Duane Soisson <dsoisson@live.com>
2016-09-05 15:43:42 -0400

    Updated both calculator and calculator_spec

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,23 @@
 class Calculator
 	def add(num1, num2)
 		num1 + num2
+	end		
+	def subtract(num1, num2)
+		num1 - num2
 	end
+	def multiply(num1, num2)
+		num1 * num2
+	end
+	#try with catching error
+	def divide(num1, num2)
+		if num2 == 0
+			puts "You can't divide by zero."
+		else
+			num1/num2
+		end
+	end
+	#try without catching error
+# 	def divide(num1, num2)
+# 		num1/num2
+# 	end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -13,4 +13,49 @@ describe Calculator do
 			expect(calc.add(12, 0)).to eq 12
 		end
 	end
+	describe 'subtraction' do
+		it 'cares about the order numbers are subtracted' do
+			calc = Calculator.new
+			expect(calc.subtract(5, 9)).to eq -4
+			expect(calc.subtract(9, 5)).to eq 4
+		end
+		it 'does not change when you subtract 0' do
+			calc = Calculator.new
+			expect(calc.subtract(12, 0)).to eq 12
+		end
+	end
+	describe 'multiplication' do
+		it 'does not care about the order numbers are multiplied' do
+			calc = Calculator.new
+			expect(calc.multiply(5, 9)).to eq 45
+			expect(calc.multiply(9, 5)).to eq 45
+		end
+		it 'should be 0 when you multiply by 0' do
+			calc = Calculator.new
+			expect(calc.multiply(12, 0)).to eq 0
+		end
+	end
+	describe 'division' do
+		it 'cares about the order numbers are divided' do
+			calc = Calculator.new
+			expect(calc.divide(5.to_f, 10.to_f)).to eq 0.5
+			expect(calc.divide(10, 5)).to eq 2
+		end
+		
+		it 'gives an error when divided by 0' do
+			calc = Calculator.new
+			begin
+			  	 # Try to divide by zero.
+				i = calc.divide(12,0)
+			rescue ZeroDivisionError
+			    # Handle the error.
+			    puts "Programming Error: Division By Zero"
+			end
+
+			# Display final value.
+		puts i
+
+		end
+	end
+
 end
