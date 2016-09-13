f5c85a50e9dc17bebe02494d1d9a74c49a56a0e6
Mr-Cotton <colton.adkins56@yahoo.com>
2016-09-13 15:37:23 -0700

    Add files via upload

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,19 @@
 class Calculator
 	def add(num1, num2)
 		num1 + num2
+	end	
+	def sub(num1, num2)
+		num1 - num2
 	end
+	def multiply(num1, num2)
+		num1 * num2
+	end
+	def divide(num1, num2)
+			num1 / num2
+			if num2 == 0 
+				puts "You can't divide by zero."
+			else
+				num1 / num2
+			end	
+	end		
 end
