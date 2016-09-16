46e4d4681a7a81542d86713b2a29f33ab705e13a
Tim Glover <tglover9@student.cscc.edu>
2016-09-16 15:52:49 -0400

    assignment1

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,17 @@ class Calculator
 	def add(num1, num2)
 		num1 + num2
 	end
+	def subtract(num1, num2)
+		num1 - num2
+	end
+	def multiply(num1, num2)
+		num1 * num2
+	end
+	def divide(num1, num2)
+		if num2 == 0
+			nil
+		else
+			num1 / num2
+		end	
+	end
 end
