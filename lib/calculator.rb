afac8ff09a7ce4d26b35b22f0f0fafcf42f6bf86
sarah-mcculley <smcculle@gmail.com>
2016-10-12 22:28:31 -0400

    calculator and tests

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,33 @@
 class Calculator
-	def add(num1, num2)
-		num1 + num2
+	def add(*nums)
+		result = 0
+		nums.each do |x|
+			result += x
+		end
+		return result
 	end
+
+	def subtract(*nums)
+		result = nums[0]
+		nums[1..-1].each do |x|
+			result -= x
+		end
+		return result
+	end
+
+	def multiply(*nums)
+		result = 1
+		nums.each do |x|
+			result *= x
+		end
+		return result
+	end
+
+	def divide(*nums)
+			result = nums[0]
+			nums[1..-1].each do |x|
+				result /= x
+			end
+			return result
+		end
 end
